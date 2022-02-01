eca59dc969a1ac542d42d70472908e133d28e00b
Ipse-lachiquita <ipse.0116@gmail.com>
2022-02-01 15:43:45 +0900

    Initial commit

---
+++ app/src/main/java/com/ipselachiquita/bottomnavigationviewwithnavigation/fragments/FriendsFragment.kt
@@ -0,0 +1,30 @@
+package com.ipselachiquita.bottomnavigationviewwithnavigation.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ipselachiquita.bottomnavigationviewwithnavigation.databinding.FragmentFriendsBinding
+import com.ipselachiquita.bottomnavigationviewwithnavigation.databinding.FragmentHomeBinding
+
+class FriendsFragment : Fragment() {
+
+    private var mBinding : FragmentFriendsBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentFriendsBinding.inflate(inflater, container, false)
+
+        mBinding = binding
+        return mBinding?.root
+    }
+
+    override fun onDestroyView() {
+        mBinding = null
+        super.onDestroyView()
+    }
+}
